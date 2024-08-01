820b71b4f1ab8e6a598e121ea900a1582fef0329
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-08-01 16:34:33 +0900

    refactor: swagger cors 에러 해결

--- src/main/java/com/example/toyou/ToYouApplication.java
+++ src/main/java/com/example/toyou/ToYouApplication.java
@@ -1,5 +1,7 @@
 package com.example.toyou;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@OpenAPIDefinition(servers = {@Server(url = "https://to-you.store", description = "Default Server URL")})
 public class ToYouApplication {
 
 	public static void main(String[] args) {
