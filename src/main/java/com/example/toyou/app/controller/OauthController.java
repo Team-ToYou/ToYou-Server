53686d7cf8c0544b15deb34a1600134255afde93
yueunfive <122276414+yueunfive@users.noreply.github.com>
2024-08-16 12:44:19 +0900

    docs: issue template 변경

--- src/main/java/com/example/toyou/app/controller/OauthController.java
+++ src/main/java/com/example/toyou/app/controller/OauthController.java
@@ -10,8 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @Validated
 @Slf4j
-@Tag(name = "Oauth", description = "Oauth 관련 API입니다.")
+@Tag(name = "Auth", description = "Auth 관련 API입니다.")
 public class OauthController {
 
     private final OauthService oauthService;
