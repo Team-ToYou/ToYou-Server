47eae3398acd7071dc87a64d7902aaa3908c2fae
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-01-23 10:15:31 +0900

    refactor: @NotEmpty to @NotNull

--- src/main/java/com/example/toyou/app/dto/FriendRequestRequest.java
+++ src/main/java/com/example/toyou/app/dto/FriendRequestRequest.java
@@ -2,27 +2,28 @@ package com.example.toyou.app.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class FriendRequestRequest {
 
     @Getter
     public static class createFriendRequestDTO{
-        @NotEmpty
+        @NotNull
         @Schema(description = "유저 ID", nullable = false, example = "1")
         private Long userId;
     }
 
     @Getter
     public static class deleteFriendRequestDTO{
-        @NotEmpty
+        @NotNull
         @Schema(description = "유저 ID", nullable = false, example = "1")
         private Long userId;
     }
 
     @Getter
     public static class acceptFriendRequestDTO{
-        @NotEmpty
+        @NotNull
         @Schema(description = "유저 ID", nullable = false, example = "1")
         private Long userId;
     }
