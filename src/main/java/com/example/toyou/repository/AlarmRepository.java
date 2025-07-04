03842c93f3ccbcee3ee74a30db65e895cbbd909d
yueunfive <122276414+yueunfive@users.noreply.github.com>
2025-07-04 17:48:40 +0900

    feat: k6 script 작성

--- src/main/java/com/example/toyou/repository/AlarmRepository.java
+++ src/main/java/com/example/toyou/repository/AlarmRepository.java
@@ -1,13 +1,10 @@
 package com.example.toyou.repository;
 
 import com.example.toyou.domain.Alarm;
-import com.example.toyou.domain.FriendRequest;
-import com.example.toyou.domain.Question;
 import com.example.toyou.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
